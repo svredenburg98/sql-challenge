d4e03fd78cf2fa508cf6e94f1f016b41009fc35b
svredenburg98 <66885962+svredenburg98@users.noreply.github.com>
2020-10-07 21:38:53 -0500

    finished sales query

--- EmployeeSQL/sql files/query.sql
+++ EmployeeSQL/sql files/query.sql
@@ -24,3 +24,11 @@ FROM employees
 SELECT first_name, last_name, sex
 FROM employees
 WHERE first_name = 'Hercules' AND last_name LIKE 'B%'
+
+SELECT employees.emp_no, last_name, first_name, dept_name
+FROM employees
+	JOIN dept_emp
+	ON employees.emp_no = dept_emp.emp_no
+	JOIN departments
+	ON departments.dept_no = dept_emp.dept_no
+WHERE dept_name = 'Sales'
