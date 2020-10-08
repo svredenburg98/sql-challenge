c56b3444a4e00d855cb9fa682323709c5b9cb865
svredenburg98 <66885962+svredenburg98@users.noreply.github.com>
2020-10-07 21:34:42 -0500

    finished employee department query

--- EmployeeSQL/sql files/query.sql
+++ EmployeeSQL/sql files/query.sql
@@ -6,3 +6,17 @@ ON employees.emp_no = salaries.emp_no
 SELECT emp_no, first_name, last_name, hire_date
 FROM employees
 WHERE hire_date LIKE '%1986'
+
+SELECT dept_manager.dept_no, dept_name, employees.emp_no, first_name, last_name
+FROM employees
+	JOIN dept_manager
+	ON employees.emp_no = dept_manager.emp_no
+	JOIN departments
+	ON departments.dept_no = dept_manager.dept_no
+
+SELECT dept_name, employees.emp_no, first_name, last_name
+FROM employees
+	JOIN dept_emp
+	ON employees.emp_no = dept_emp.emp_no
+	JOIN departments
+	ON departments.dept_no = dept_emp.dept_no
