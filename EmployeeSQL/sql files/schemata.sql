3d120e0c9583e1bdddeda71c941c0a31a8e6e261
svredenburg98 <66885962+svredenburg98@users.noreply.github.com>
2020-10-07 20:29:02 -0500

    fixed table to add a foreign key connection between employees and titles

--- EmployeeSQL/sql files/schemata.sql
+++ EmployeeSQL/sql files/schemata.sql
@@ -1,11 +1,13 @@
 CREATE TABLE employees (
 	emp_no INT PRIMARY KEY,
 	emp_title_id VARCHAR(30),
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id),
 	birth_date DATE,
 	first_name VARCHAR(30),
 	last_name VARCHAR(30),
 	sex CHAR,
 	hire_date DATE)
+	
 	
 CREATE TABLE salaries (
 	emp_no INT,
@@ -31,3 +33,5 @@ CREATE TABLE dept_emp (
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
 	dept_no VARCHAR(10),
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no))
+	
+DROP TABLE employees, dept_emp, dept_manager, titles, salaries, departments
